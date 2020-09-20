a40e124ed0ba16feb3358dfb424b6f3b4c3ccf7e
Qadriddin <qadrdin81@gmail.com>
2020-09-20 21:55:42 +0500

    stats

---
+++ pkg/bank/stats/stats.go
@@ -0,0 +1,25 @@
+import stats
+
+import (
+	"github.com/Qadriddin-dev/card/pkg/bank/types"
+)
+
+func Avg(payments []types.Payment) types.Money {
+	var mid types.Money
+	for _, payment := range payments {
+		mid += payment.Amount
+	}
+	length := len(payments)
+	mid /= types.Money(length) 
+	return mid
+}
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	var total types.Money
+	for _, payment := range payments {
+		if payment.Category == category {
+			total += payment.Amount
+		}
+	}
+	return total
+}
