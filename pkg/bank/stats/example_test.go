5c77e4f4431b0446a5d6240c8a3e4878ad545381
Qadriddin <qadrdin81@gmail.com>
2020-09-20 21:59:57 +0500

    stats

---
+++ pkg/bank/stats/example_test.go
@@ -0,0 +1,55 @@
+import  stats
+
+import {
+	"github.com/Qadriddin-dev/card/pkg/bank/types"
+	"fmt"
+}
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+		  ID:2,
+		  Amount:50_00,
+		  Category: "Cat",
+		},
+		{
+		  ID:1,
+		  Amount:52_00,
+		  Category: "Cat",
+		},
+		{
+		  ID:3,
+		  Amount:52_00,
+		  Category: "Cat",
+		},
+	  }
+	
+	  fmt.Println(Avg(payments))
+	
+	  //Output: 5133
+	
+}
+
+func ExampleTotalInCategory(){
+	payments := []types.Payment{
+	  {
+		ID:2,
+		Amount:53_00,
+		Category: "Cafe",
+	  },
+	  {
+		ID:1,
+		Amount:51_00,
+		Category: "Cafe",
+	  },
+	  {
+		ID:3,
+		Amount:52_00,
+		Category: "Restaurant",
+	  },
+	}
+  
+	fmt.Println(TotalInCategory(payments, "Cafe"))
+  
+	//Output: 10400
+  }
